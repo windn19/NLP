386b370f4e4c93a8ec384e1f1ab7d95daa5900d1
dmitry <windn12@gmail.com>
2023-11-24 00:14:11 +0500

    lesson2 edit app

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import io
+
+from PIL import Image
 import streamlit as st
 
 from models import load_model, translate
@@ -8,7 +11,7 @@ def load_image():
     if uploaded_file is not None:
         image_data = uploaded_file.getvalue()
         st.image(image_data)
-        return uploaded_file
+        return Image.open(io.BytesIO(image_data))
 
 
 model = load_model()
@@ -17,4 +20,4 @@ img = load_image()
 result = st.button('Распознать изображение')
 if result:
     st.write('**Результат распознавания:**')
-    print(translate(*model, img))
+    translate(*model, img)
