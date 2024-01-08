56c36b7cdf076e56f81c661c4dbf08adc9c6c49a
Dmitry Noskov <windn12@gmail.com>
2024-01-09 00:46:18 +0500

    Update models.py

--- models.py
+++ models.py
@@ -6,10 +6,10 @@ import streamlit as st
 def load_model():
     image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
     # translate = pipeline('translation_en_to_ru', model='Helsinki-NLP/opus-mt-en-ru')
-    return image_to_text# , translate
+    return image_to_text # , translate
 
 
-def translate(img2text, trans, img):
+def translate(img2text, img):
     texts = img2text(img)[0]
     # return trans(texts['generated_text'])
     return texts['generated_text']
